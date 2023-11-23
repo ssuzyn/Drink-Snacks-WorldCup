336f7f2346c9854bf8dd9c33b5de9b39add2f8d6
ssuzyn <asj7090@gmail.com>
2023-11-23 17:05:44 +0900

    ✨ [FEAT] 앱 시작 화면 구현

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton btnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnStart = (ImageButton)findViewById(R.id.btnStart);
+
+        btnStart.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, RoundActivity.class);
+            startActivity(intent);
+        });
     }
 }

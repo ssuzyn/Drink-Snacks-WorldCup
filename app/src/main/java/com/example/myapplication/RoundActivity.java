c1e39289fea7b93ec95368307993ffd8e68e5aeb
ssuzyn <asj7090@gmail.com>
2023-11-23 17:06:56 +0900

    ✨ [FEAT] 안주 월드컵 라운드 선택 화면 구현

---
+++ app/src/main/java/com/example/myapplication/RoundActivity.java
@@ -0,0 +1,44 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RoundActivity extends AppCompatActivity {
+
+    ImageButton btnRound16, btnRound8, btnRound4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_round);
+
+        btnRound16 = (ImageButton) findViewById(R.id.btnRound16);
+        btnRound8 = (ImageButton) findViewById(R.id.btnRound8);
+        btnRound4 = (ImageButton) findViewById(R.id.btnRound4);
+
+        btnRound16.setOnClickListener(v -> {
+            Intent intent = new Intent(RoundActivity.this, WorldCupActivity.class);
+            intent.putExtra("ROUND_NUMBER", 16); // 16 전달
+            startActivity(intent);
+        });
+
+        btnRound8.setOnClickListener(v -> {
+            Intent intent = new Intent(RoundActivity.this, WorldCupActivity.class);
+            intent.putExtra("ROUND_NUMBER", 8); // 8 전달
+            startActivity(intent);
+        });
+
+        btnRound4.setOnClickListener(v -> {
+            Intent intent = new Intent(RoundActivity.this, WorldCupActivity.class);
+            intent.putExtra("ROUND_NUMBER", 4); // 4 전달
+            startActivity(intent);
+        });
+
+
+
+    }
+}

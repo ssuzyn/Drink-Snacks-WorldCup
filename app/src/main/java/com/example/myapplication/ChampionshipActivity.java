b73129d4381cf6a83101e49e7a7ce6870e468479
ssuzyn <asj7090@gmail.com>
2023-12-04 17:45:42 +0900

    ✨ [FEAT] 최종으로 선택된 메뉴 출력 기능 추가

---
+++ app/src/main/java/com/example/myapplication/ChampionshipActivity.java
@@ -0,0 +1,45 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ChampionshipActivity extends AppCompatActivity {
+
+    private ImageView championMenuImage;
+    private ImageButton btnBarList, btnHome;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_championship);
+
+        championMenuImage = (ImageView) findViewById(R.id.championImage);
+        btnBarList = (ImageButton) findViewById(R.id.btnBarList);
+        btnHome = (ImageButton) findViewById(R.id.btnHome);
+
+        MenuInfo winningMenu = (MenuInfo) getIntent().getSerializableExtra("WinningMenu");
+        if (winningMenu != null) {
+            Log.d("Championship", "최종 우승 메뉴 : " + winningMenu.getMenuName());
+
+            int resourceId = ImageResourceHelper.getImageResourceId(this, winningMenu.getImagePath());
+            championMenuImage.setImageResource(resourceId);
+            championMenuImage.invalidate();
+        }
+
+        btnBarList.setOnClickListener(view -> {
+            Intent intent = new Intent(ChampionshipActivity.this, BarListActivity.class);
+            intent.putExtra("menu", winningMenu.getMenuName());
+            startActivity(intent);
+        });
+
+        btnHome.setOnClickListener(view -> {
+            Intent intent = new Intent(ChampionshipActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
+    }
+}
